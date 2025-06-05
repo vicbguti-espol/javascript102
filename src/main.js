230f22c78cced256aae7eb368230e4bf23ce7b87
Allan <aavendan@gmail.com>
2025-06-05 15:28:29 -0500

    init

---
+++ src/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const itemData = {
+    item1: {
+        name: 'Finalista 01',
+        image: 'https://picsum.photos/seed/animal/250/200',
+        photographer: 'John Doe',
+        description: ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        score: 42
+    },
+    item2: {
+        name: 'Finalista 2',
+        image: 'https://picsum.photos/seed/beach/250/200',
+        photographer: 'Jane Smith',
+        description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        score: 84
+    },
+    item3: {
+        name: 'Finalista 3',
+        image: 'https://picsum.photos/seed/mountain/250/200',
+        photographer: 'Alice Johnson',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        score: 36
+    }
+};
